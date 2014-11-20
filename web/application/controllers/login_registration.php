c700ae1fd9fe8fce06b5999832083ad30fc095ec
Ziker22 <juraj.slavicek@gmail.com>
2014-11-20 02:03:28 +0100

    Pridane komentare

--- web/application/controllers/login_registration.php
+++ web/application/controllers/login_registration.php
@@ -1,24 +1,39 @@
 <?php
 class Login_Registration extends CI_controller
 {
-
+/**
+* Funckcia zobrazi view login s prihlasovacim formularom
+*/
 public function login()
 {
 $this->load->view('login');
 }
-
+/**
+* Funckcia zobrazi view register s registracnym formularom
+*/
 public function register()
 {
 $this->load->view('register');
 }
-
+/**
+* Funkcia vyuziva standardnu hashovaciu funkciu md5 na hashovanie pouzivatelskeho hesla
+* pred vlozenim do databazy
+* @param string rawPasword heslo v cistom plain tvare vytiahnute z formulara
+* @param string salt 32 bitovy nahodne generovany retazec zvysujuci bezpecnost hesla
+* @return string hashedPassword zahesovane heslo
+*/
 public function hash_password($rawPassword,$salt){
 	$hashedPassword= md5($rawPassword.$salt);
 
 	return $hashedPassword;
 
 }
-
+/**
+* Funkcia generuje nahodny x bitovy retazec tzv salt ktore znemoznuje prelamovanie hesla pomocou duhovych tabuliek
+* retazec je zlozeny z vopred zadanych ASCII znakov
+* @param string max maximalna dlzka generovaneho retazca
+* @return string salt vygenerovany max znakovy retazec
+*/
 public function generate_salt($max){
 	   $characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?";
         $i = 0;
@@ -29,7 +44,10 @@ public function generate_salt($max){
         }
         return $salt;
 }
-
+/**
+* Funkcia parsuje udaje zadane pouzivatelom vo formulari zavola modelovu funkciu na ulozenie udajov do databazt
+* a na zaver zobrazi udaj o uspesnosti registracie
+*/
 public function add_user(){
 	
 $email=$_POST['email'];
@@ -46,7 +64,10 @@ $this->model->save_user('DEFAULT',$name,$surname,$email,$hashed_password,$salt);
 $this->load->view('registration_successful');
 
 }
-
+/**
+* Funkcia overujuca spravnost zadanych prihlasovacich udajov na zaklade udajov poskytnutych
+* v prihlasovacom formulari pokial su spravne zobrzi view o uspesnosti prihlasenia v opacnom pripade o neuspesnosti
+*/
 public function authentificate_user(){
 	$email=$_POST['email'];
 	$typed_password=$_POST['password'] ;
--- web/application/models/login_registration_model.php
+++ web/application/models/login_registration_model.php
@@ -7,7 +7,11 @@ class Ulovok_model extends CI_Model
 	{
 		parent::__construct();
 	}
-
+/**
+* Modelova funkcia na ziskavanie hashovaneho hesla z databazy na zaklade pouzivatelskeho emailu
+* @param string email pouzivatelsky email na zaklade ktoreho vyhladame heslo
+* @return string result vysledok query do databazy
+*/
 	function get_password_for_login($email){
 
 		$this->db->where('email', $email);
@@ -18,7 +22,15 @@ class Ulovok_model extends CI_Model
 		// spustenie query a vratenie hodnoty funkciou
 		return $query->result();
 	}
-
+/**
+* Modelova funkcia nukladajuca uzivatela na zaklade zadanych parametrov
+* @param int - id unikatny identifikator v databaze
+* @param string name realne meno pouzivatela
+* @param string surname realne priezvisko pouzivatela
+* @param string email pouzivatelsky email
+* @param string password hashovane pouzivatelske heslo
+* @param string salt snahodne vygenerovany retazec unikatny pre pouzivatela
+*/
 	function save_user($id,$name,$surname,$email,$password,$salt){
 	$data = array(
 			'id' 		      => $id,
