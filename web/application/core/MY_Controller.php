3ff70a8d68471a150287b3cd705316238089a85e
Richard Filipčík <richard.filipcik@gmail.com>
2015-03-16 16:55:07 +0100

    fixed device type based on HTTP User Agent

--- web/application/core/MY_Controller.php
+++ web/application/core/MY_Controller.php
@@ -9,11 +9,15 @@ class MY_Controller extends CI_Controller
         parent::__construct();
 
         $this->load->model('Online_user_model');
-        $ret = (bool)preg_match('#\b(ip(hone|od|ad)|android|opera m(ob|in)i|windows (phone|ce)|blackberry|tablet'.
-        '|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp|laystation portable)|nokia|fennec|htc[\-_]'.
-        '|mobile|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $_SERVER['HTTP_USER_AGENT']);
+        
+        if (array_key_exists('HTTP_USER_AGENT', $_SERVER))
+        {
+            $ret = (bool)preg_match('#\b(ip(hone|od|ad)|android|opera m(ob|in)i|windows (phone|ce)|blackberry|tablet'.
+            '|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp|laystation portable)|nokia|fennec|htc[\-_]'.
+            '|mobile|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $_SERVER['HTTP_USER_AGENT']);
 
-        self::$type = $ret ? 'm' : 'w';
+            self::$type = $ret ? 'm' : 'w';
+        }
         
         @session_start();
     }
