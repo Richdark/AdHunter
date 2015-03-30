af08f537f812f5689e832832c42cbce7a0e7acb4
Richard Filipčík <richard.filipcik@gmail.com>
2015-03-30 23:37:00 +0200

    user profile basic styling

--- web/application/controllers/profile.php
+++ web/application/controllers/profile.php
@@ -4,7 +4,9 @@ class Profile extends MY_Controller
 {
 	public function index()
 	{
-		//
+		$vars['page_title']   = 'Nastavenia účtu';
+		$vars['profile_menu'] = $this->load->view('profile_menu', NULL, true);
+		$this->load->template('profile_main', $vars);
 	}
 }
 
---
+++ web/application/views/profile_main.php
@@ -0,0 +1,7 @@
+<?php echo $profile_menu; ?>
+
+<div class="profile_content">
+	<p>Nejaký super obsah...</p>
+</div>
+
+<div class="clear"></div>
