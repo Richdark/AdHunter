fe3757e9964510e9b9d8f1d5b0098335aa94464c
sanoferko <sano.ferencik@gmail.com>
2015-05-12 13:51:20 +0200

    uprava menu

---
+++ web/application/views/profile_menu.php
@@ -0,0 +1,9 @@
+<nav class="profile">
+	<ul>
+		<li><a href="<?php echo root_url(); ?>profile/"<?php if ($this->uri->segment(2) == '') { echo ' class="current"'; } ?>>Nastavenia účtu</a></li>
+		<li><a href="<?php echo root_url(); ?>profile/catches/"<?php if ($this->uri->segment(2) == 'ads') { echo ' class="current"'; } ?>>Moje úlovky</a></li>
+		<li><a href="<?php echo root_url(); ?>profile/badges/"<?php if ($this->uri->segment(2) == 'badges') { echo ' class="current"'; } ?>>Moje ocenenia</a></li>
+		<li><a href="<?php echo root_url(); ?>profile/statistics/"<?php if ($this->uri->segment(2) == 'statistics') { echo ' class="current"'; } ?>>Štatistiky</a></li>
+		<li><a href="<?php echo root_url(); ?>profile/merges/"<?php if ($this->uri->segment(2) == 'merges') { echo ' class="current"'; } ?>>Zlučovanie bilbordov</a></li>
+	</ul>
+</nav>
