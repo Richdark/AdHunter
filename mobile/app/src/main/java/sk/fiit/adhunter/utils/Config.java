2c988d6f52139c54628c37b000db52b2d79c0900
jerry33333 <jaroslav.zigo@garwan.sk>
2015-05-25 23:47:27 +0200

    Changed from devel to production endpoint

--- mobile/app/src/main/java/sk/fiit/adhunter/utils/Config.java
+++ mobile/app/src/main/java/sk/fiit/adhunter/utils/Config.java
@@ -11,7 +11,7 @@ import retrofit.RestAdapter;
 public class Config {
 
 //    public static final String ENDPOINT_URL = "http://team14-14.ucebne.fiit.stuba.sk/adhunter"; // test
-    public static final String ENDPOINT_URL = "http://devel.adhunter.eu";
+    public static final String ENDPOINT_URL = "http://adhunter.eu";
     public static final RestAdapter.LogLevel LOG_LEVEL = RestAdapter.LogLevel.FULL;
     public static String DEVICE_ID;
 
