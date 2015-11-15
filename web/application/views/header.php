68a4f3b32928340120869c9a6bcaca638f8eb0fd
Tomas <aspintm@azet.sk>
2015-11-15 22:38:39 +0100

    responzivna verzia 2

--- web/application/views/header.php
+++ web/application/views/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title>AdHunter<?php if (isset($page_title)){ echo ' - '. $page_title; } ?></title>
 
